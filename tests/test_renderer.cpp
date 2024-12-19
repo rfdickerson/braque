f4d2d5cdd7f7a7b26538b4e887cf3bcc37d6af8d
Robert F. Dickerson <rfdickerson@gmail.com>
2024-12-18 21:28:46 -0600

    add window test

--- tests/test_renderer.cpp
+++ tests/test_renderer.cpp
@@ -5,10 +5,18 @@
 // tests/test_renderer.cpp
 #include "gtest/gtest.h"
 #include "braque/renderer.hpp"
+#include "braque/window.hpp"
 
 TEST(RendererTest, Initialization) {
     braque::Renderer renderer;
     // ... your assertions to test renderer initialization ...
     auto device = renderer.getDevice();
     EXPECT_TRUE(device);
+}
+
+TEST(WindowTest, Initialization) {
+    braque::Window window;
+    // ... your assertions to test window initialization ...
+    auto nativeWindow = window.getNativeWindow();
+    EXPECT_TRUE(nativeWindow);
 }
