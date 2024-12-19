951fdd7211aaa4771b044298c90c5e2eb04bb319
Robert F. Dickerson <rfdickerson@gmail.com>
2024-12-19 11:45:48 -0600

    swapchain

--- engine/src/swapchain.cpp
+++ engine/src/swapchain.cpp
@@ -7,7 +7,7 @@
 #include <spdlog/spdlog.h>
 
 namespace braque {
-Swapchain::Swapchain(Window &window, Renderer &renderer): renderer(renderer) {
+Swapchain::Swapchain(Window &window, Renderer &renderer) : renderer(renderer), swapchainFormat(vk::Format::eUndefined) {
     createSwapchain(window);
     createSemaphores();
     createFences();
@@ -87,7 +87,8 @@ void Swapchain::acquireNextImage() {
     acquireNextImageInfo.setSwapchain(swapchain);
     acquireNextImageInfo.setTimeout(UINT64_MAX);
     acquireNextImageInfo.setSemaphore(imageAvailableSemaphores[currentFrameInFlight]);
-    acquireNextImageInfo.setDeviceMask(1);;
+    acquireNextImageInfo.setDeviceMask(1);
+    ;
 
     auto result = renderer.getDevice().acquireNextImage2KHR(acquireNextImageInfo);
 
@@ -180,9 +181,7 @@ void Swapchain::createFences() {
     imagesInFlight.resize(imageCount, VK_NULL_HANDLE);
 }
 
-void Swapchain::createSwapchainImages() {
-    swapchainImages = renderer.getDevice().getSwapchainImagesKHR(swapchain);
-}
+void Swapchain::createSwapchainImages() { swapchainImages = renderer.getDevice().getSwapchainImagesKHR(swapchain); }
 
 void Swapchain::createCommandBuffers() {
     // create the command pool
@@ -254,4 +253,4 @@ void Swapchain::recordFrameLatency() {
     // get the duration
 }
 
-} // braque
+} // namespace braque
